ba2fe3e23615065e20b88a430538a1ae8b23e4c0
ebisuke <fungi@rect.sakura.ne.jp>
2021-06-20 11:29:28 +0900

    create cmdunipf

---
+++ tpIpfToolCmd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using tpIpfTool;
+
+namespace tpIpfToolCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 1)
+            {
+                Console.WriteLine("Need more arguments.");
+                return;
+            }
+            IpfPack pck = new IpfPack((string s) =>
+            {
+                Console.WriteLine(s);
+            });
+            pck.ExtIpf(args.Skip(1).ToArray());
+        }
+    }
+}
